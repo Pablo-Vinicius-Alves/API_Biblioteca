70726da062f6594ecfff39079e691a6953c367d6
PABLO VINÍCIUS ALVES PISOLATO <26171627@edu119.local>
2026-09-10 14:28:15 -0300

    Add front end

---
+++ app.js
@@ -0,0 +1,13 @@
+async function carregarLivros(){
+    const resposta = await fetch('http://localhost:3000/livros');
+    const dados = await resposta.json();
+    console.log(dados)
+    const divLivros = document.getElementById('lista-livros');
+    const listaHtml = dados.map(livro => {
+        return `<p>${livro.nome} - ${livro.autor} - ${livro.status}</p>`;
+    });
+
+    divLivros.innerHTML = listaHtml.join('');
+}
+
+carregarLivros();
