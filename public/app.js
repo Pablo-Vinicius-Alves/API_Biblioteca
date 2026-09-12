a8ff83e297e9a943e08cf1fcd110cac2c381bf6e
Pablo Vinícius Alves Pisolato <pblalves29@gmail.com>
2026-09-12 16:36:31 -0300

    add css

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ async function carregarLivros(){
     const resposta = await fetch('http://localhost:3000/livros');
     const dados = await resposta.json();
     console.log(dados)
-    const divLivros = document.getElementById('lista-livros');
+    const divLivros = document.getElementById('campo');
     const listaHtml = dados.map(livro => {
         return `<p>${livro.nome} - ${livro.autor} - ${livro.status}</p>`;
     });
